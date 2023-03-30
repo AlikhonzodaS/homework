af4479236a4b2e71c843e9885a3375cecf5365bf
unknown <alikhonzodasalmon07@gmail.com>
2023-03-30 15:41:35 +0300

    first commit

--- ifExercise/if30.go
+++ ifExercise/if30.go
@@ -6,12 +6,10 @@ import "fmt"
 //и т. д
 
 func IfEx30(a int) {
-	if a > 99 && a < 1000 {
-		fmt.Println("Our number has three number")
-	} else if a < 100 {
-		fmt.Println("Our number has two number")
-	} else if a%2 == 0 {
-		fmt.Println("Our number is plus")
+	if a > 99 && a < 1000 && a%2 == 0 {
+		fmt.Print("Our number has three number\nOur number is plus")
+	} else if a < 100 && a%2 == 0 {
+		fmt.Println("Our number has two number\nOur number is plus")
 	} else if a == 0 {
 		fmt.Println("Our number is zero")
 	} else {
